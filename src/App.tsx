ef8fb65e9cd9ced28d13b6c2d517751abe01d324
Pedro R Gonçalves <pribeiro.goncalves@gmail.com>
2020-12-30 17:06:43 +0000

    test commiting with tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Logo />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
