55913a4685e6230b8b6adbb7bdbb0837536cacda
Pedro R Gonçalves <pribeiro.goncalves@gmail.com>
2020-12-30 17:08:16 +0000

    add failing test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,4 +6,6 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+
+  expect(true).toBe(false);
 });
