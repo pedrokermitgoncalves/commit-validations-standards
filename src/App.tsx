c6900ee263124a440530abdb8ce440301654162b
Pedro R Gonçalves <pribeiro.goncalves@gmail.com>
2020-12-31 11:59:15 +0000

    refactor: change link string
    ISSUES CLOSED: 123

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learning React
+          Learned react
         </a>
       </header>
     </div>
