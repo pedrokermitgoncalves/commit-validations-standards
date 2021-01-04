b7126f7eb2e07f7edeeee2b9f4626da0fadfebca
Pedro R Gonçalves <pribeiro.goncalves@gmail.com>
2021-01-04 17:10:35 +0000

    chore: demo

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learned react
+          Learning react
         </a>
       </header>
     </div>
