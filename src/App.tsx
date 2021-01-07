5e3926c53bf9ed92c57c261a1a58a8ab7a08cbea
Pedro R Gonçalves <pribeiro.goncalves@gmail.com>
2021-01-07 11:30:07 +0000

    feat: changing some string
    this is the body of my message

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learning react
+          Learn react
         </a>
       </header>
     </div>
